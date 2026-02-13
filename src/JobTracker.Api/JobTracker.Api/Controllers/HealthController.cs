28c2756ec3f3feb8e6165407f493a32b41e799fc
jomitho <jonathanthorsteinsson@hotmail.com>
2026-02-13 20:08:54 +0100

    feat: initial API setup with Swagger and health endpoint

---
+++ src/JobTracker.Api/JobTracker.Api/Controllers/HealthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JobTracker.Api.Controllers;
+
+[ApiController]
+[Route("health")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new {status = "ok"});
+    }
+}
